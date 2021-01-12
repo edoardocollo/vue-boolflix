c21c7587cc6688138dce1b69deea3982ccdc7bba
edoardocollo <edoardo.collo@gmail.com>
2021-01-12 14:43:34 +0100

    added tv api request

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,15 @@ let app = new Vue({
   },
   methods: {
     getSearch: function(search) {
+
+
+
+
+
+
+
+
+
       var config = {
         method: 'get',
         url: `https://api.themoviedb.org/3/search/movie?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`,
@@ -45,7 +54,81 @@ let app = new Vue({
         .catch(function(error) {
           console.log(error);
         });
+
+
+
+        var config2 = {
+          method: 'get',
+          url: `https://api.themoviedb.org/3/search/tv?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`,
+          headers: {}
+        };
+
+        axios(config2)
+          .then(function(response) {
+            const data = response.data.results;
+            console.log(data);
+            data.forEach(e=>{
+              let votoTemp = Math.round(e.vote_average/2);
+              let votoStarsActive = [];
+              let votoStarsRest = [];
+              for (var i = 0; i < votoTemp; i++) {
+                votoStarsActive.push('#');
+              }
+              for (var i = 0; i < (5 - votoTemp); i++) {
+                votoStarsRest.push('#')
+              }
+              app.searchResults.push({
+                titolo: e.name,
+                titoloOriginale: e.original_name,
+                linguaOriginale: e.original_language,
+                voto: Math.round(e.vote_average/2),
+                stelle: votoStarsActive,
+                stelleRest: votoStarsRest,
+                poster: e.poster_path,
+
+
+              });
+            });
+
+
+
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+
+
+
+
     },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     getPosterURL: function(path){
       const fullPath = 'https://image.tmdb.org/t/p/w500'+path;
       return fullPath;
