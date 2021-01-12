483f826ce89f0c695548494e152e33ad717d5bac
edoardocollo <edoardo.collo@gmail.com>
2021-01-12 11:20:17 +0100

    log search on click

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    searchText:'',
+    searchResults:'',
+  },
+  methods: {
+    getSearch: function(search) {
+      var config = {
+        method: 'get',
+        url: `https://api.themoviedb.org/3/search/movie?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`,
+        headers: {}
+      };
+
+      axios(config)
+        .then(function(response) {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+
+  },
+  mounted() {
+
+  }
+});
