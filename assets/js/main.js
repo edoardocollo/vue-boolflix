7b061c70f805451bbbed6990a49f12335391a15e
edoardocollo <edoardo.collo@gmail.com>
2021-01-12 14:21:59 +0100

    added stars and poster

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,41 @@ let app = new Vue({
   el: '#app',
   data: {
     searchText:'',
-    searchResults:'',
+    searchResults:[],
   },
   methods: {
     getSearch: function(search) {
       var config = {
         method: 'get',
-        url: `https://api.themoviedb.org/3/search/movie?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&include_adult=false`,
+        url: `https://api.themoviedb.org/3/search/movie?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`,
         headers: {}
       };
 
       axios(config)
         .then(function(response) {
-          console.log(JSON.stringify(response.data));
-          app.searchResults = response.data.results;
-          console.log(app.searchResults);
-
+          const data = response.data.results;
+          console.log(data);
+          data.forEach(e=>{
+            let votoTemp = Math.round(e.vote_average/2);
+            let votoStarsActive = [];
+            let votoStarsRest = [];
+            for (var i = 0; i < votoTemp; i++) {
+              votoStarsActive.push('#');
+            }
+            for (var i = 0; i < (5 - votoTemp); i++) {
+              votoStarsRest.push('#')
+            }
+            app.searchResults.push({
+              titolo: e.title,
+              titoloOriginale: e.original_title,
+              linguaOriginale: e.original_language,
+              voto: Math.round(e.vote_average/2),
+              stelle: votoStarsActive,
+              stelleRest: votoStarsRest,
+              poster: e.poster_path,
+              flag: e.backdrop_path
+            });
+          });
 
 
 
@@ -25,8 +44,11 @@ let app = new Vue({
         .catch(function(error) {
           console.log(error);
         });
-    }
-
+    },
+    getURL: function(path){
+      const fullPath = 'https://image.tmdb.org/t/p/w500'+path;
+      return fullPath;
+    },
   },
   mounted() {
 
