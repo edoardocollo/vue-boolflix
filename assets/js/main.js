4fc1f50b1629f3630632deeb7bda749dd9292575
edoardocollo <edoardo.collo@gmail.com>
2021-01-18 17:41:17 +0100

    js refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,25 +5,25 @@
 let app = new Vue({
   el: '#app',
   data: {
-    sectionActive:'all',
-    genreActive:'Tutti',
+    sectionActive: 'all',
+    genreActive: 'Tutti',
     searchText: '',
     searchResultsMovie: [],
     searchResultsTV: [],
     searchResultsTotal: [],
     totalSearch: '',
-    genreMap:'',
+    genreMap: '',
     showChevron: false,
     hamburger: false,
   },
   methods: {
 
-    getGenre: function(array){
+    getGenre: function(array) {
       // console.log(array);
       let generi = [];
-      array.forEach(e=>{
+      array.forEach(e => {
         const id = e;
-        this.getGenreName(this.genreMap,generi,id);
+        this.getGenreName(this.genreMap, generi, id);
         // this.genreMap.forEach(e=>{
         //   if (e.id === id) {
         //     // console.log(e.name);
@@ -34,8 +34,8 @@ let app = new Vue({
       // console.log(generi);
       return generi;
     },
-    getGenreName: function(array,target, id){
-      array.forEach(e=>{
+    getGenreName: function(array, target, id) {
+      array.forEach(e => {
         // console.log('ciao');
         if (e.id === id) {
           // console.log(e.name);
@@ -67,55 +67,56 @@ let app = new Vue({
       }
     },
 
-    initGenreMap: function(){
+    initGenreMap: function() {
       axios
         .get(`https://api.themoviedb.org/3/genre/movie/list?api_key=c3425076f0ff558c6137588bf0383e0c&language=it`)
-        .then(resp =>{
+        .then(resp => {
           this.genreMap = resp.data.genres;
           this.genreMap.unshift({
-            id:100000000000,
+            id: 100000000000,
             name: 'Tutti',
           })
         })
         .catch()
 
     },
-    resizeCard: function(object){
+    resizeCard: function(object) {
       console.log(true);
       object.resize = !object.resize;
     },
-    getSearch:function(search){
+    getSearch: function(search) {
+      // CHEVRON BUTTON MANAGMENT
       this.showChevron = true;
-      if (search ==='') {
+      if (search === '') {
         this.showChevron = false;
 
       }
-      this.searchResultsMovie=[];
-      this.searchResultsTV= [];
-      this.searchResultsTotal= [];
+      // EMPTY THE ARRAYS
+      this.searchResultsMovie = [];
+      this.searchResultsTV = [];
+      this.searchResultsTotal = [];
 
+      // API CALL FOR MOVIES AND TV
       Promise.all(
-        [axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`),
-        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`)
-        ])
+          [axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`),
+            axios.get(`https://api.themoviedb.org/3/search/tv?api_key=c3425076f0ff558c6137588bf0383e0c&language=it&query=${search}&page=1&include_adult=false`)
+          ])
         .then(resp => {
-          // console.log(resp);
-          // this.searchResultsMovie = resp[0].data.results;
-          if (this.sectionActive==='movie'|| this.sectionActive==='all') {
+          // CREATE ARRAY FOR MOVIES IF ALL OR MOVIE CATEGORY IS ACTIVE
+          if (this.sectionActive === 'movie' || this.sectionActive === 'all') {
 
             const ResultsMovie = resp[0].data.results;
-            ResultsMovie.forEach(e=>{
-              // console.log(e.genre_ids);
+            ResultsMovie.forEach(e => {
               const generi = this.getGenre(e.genre_ids);
 
-
+              // IF GENRE IS 'tutti' PUSH ALL NEW OBJECT
               if (this.genreActive === 'Tutti') {
                 this.searchResultsMovie.push({
                   titolo: e.title,
                   titoloOriginale: e.original_title,
                   linguaOriginale: e.original_language,
                   poster: e.poster_path,
-                  voto: Math.ceil(e.vote_average/2),
+                  voto: Math.ceil(e.vote_average / 2),
                   overview: e.overview,
                   id: e.id,
                   genere: generi,
@@ -123,15 +124,15 @@ let app = new Vue({
 
 
                 });
-
-              }else{
+              // IF GENRE IS SELECTED PUSH ONLY THE OBJECTS THAT HAVE THE SELECTED GENRE
+              } else {
                 if (generi.includes(this.genreActive)) {
                   this.searchResultsMovie.push({
                     titolo: e.title,
                     titoloOriginale: e.original_title,
                     linguaOriginale: e.original_language,
                     poster: e.poster_path,
-                    voto: Math.ceil(e.vote_average/2),
+                    voto: Math.ceil(e.vote_average / 2),
                     overview: e.overview,
                     id: e.id,
                     genere: generi,
@@ -147,21 +148,22 @@ let app = new Vue({
 
             });
           }
-
-          if (this.sectionActive==='tv'|| this.sectionActive==='all') {
+          // CREATE ARRAY FOR TV IF ALL OR MOVIE CATEGORY IS ACTIVE
+          if (this.sectionActive === 'tv' || this.sectionActive === 'all') {
 
 
             const ResultsTV = resp[1].data.results;
-            ResultsTV.forEach(e=>{
+            ResultsTV.forEach(e => {
               const generi = this.getGenre(e.genre_ids);
 
+              // IF GENRE IS 'tutti' PUSH ALL NEW OBJECT
               if (this.genreActive === 'Tutti') {
                 this.searchResultsMovie.push({
                   titolo: e.name,
                   titoloOriginale: e.original_name,
                   linguaOriginale: e.original_language,
                   poster: e.poster_path,
-                  voto: Math.ceil(e.vote_average/2),
+                  voto: Math.ceil(e.vote_average / 2),
                   overview: e.overview,
                   id: e.id,
                   genere: generi,
@@ -169,15 +171,15 @@ let app = new Vue({
 
 
                 });
-
-              }else{
+                // IF GENRE IS SELECTED PUSH ONLY THE OBJECTS THAT HAVE THE SELECTED GENRE
+              } else {
                 if (generi.includes(this.genreActive)) {
                   this.searchResultsMovie.push({
                     titolo: e.name,
                     titoloOriginale: e.original_name,
                     linguaOriginale: e.original_language,
                     poster: e.poster_path,
-                    voto: Math.ceil(e.vote_average/2),
+                    voto: Math.ceil(e.vote_average / 2),
                     overview: e.overview,
                     id: e.id,
                     genere: generi,
@@ -190,28 +192,27 @@ let app = new Vue({
               }
             });
           }
-          // this.searchResultsTV = resp[1].data.results;
 
 
+          // ASSOCIATE GENRE AND CAST TO MOVIE OBJECTS
+          if (this.sectionActive === 'movie' || this.sectionActive === 'all') {
 
-          if (this.sectionActive==='movie'|| this.sectionActive==='all') {
-
-            this.searchResultsMovie.forEach(e =>{
+            this.searchResultsMovie.forEach(e => {
               const id = e.id;
               axios.get(`http://api.themoviedb.org/3/movie/${id}/casts?api_key=c3425076f0ff558c6137588bf0383e0c`)
-                .then(resp =>{
+                .then(resp => {
                   const dataCast = resp.data.cast;
                   let cast = '';
 
 
 
 
-                  if (dataCast.length >0) {
+                  if (dataCast.length > 0) {
 
                     for (var i = 0; i < dataCast.length && i < 5; i++) {
                       if (i == 0) {
                         cast += dataCast[i].name
-                      }else{
+                      } else {
 
                         cast += `, ${dataCast[i].name}`
                       }
@@ -224,27 +225,27 @@ let app = new Vue({
                   e.cast = cast;
                 })
                 .catch();
-              });
+            });
           }
+          // ASSOCIATE GENRE AND CAST TO TV OBJECTS
+          if (this.sectionActive === 'tv' || this.sectionActive === 'all') {
 
-          if (this.sectionActive==='tv'|| this.sectionActive==='all'){
-
-            this.searchResultsTV.forEach(e =>{
+            this.searchResultsTV.forEach(e => {
               const id = e.id;
               axios.get(`http://api.themoviedb.org/3/tv/${id}/credits?api_key=c3425076f0ff558c6137588bf0383e0c`)
-                .then(resp =>{
+                .then(resp => {
                   const dataCast = resp.data.cast;
                   let cast = '';
 
 
 
 
-                  if (dataCast.length >0) {
+                  if (dataCast.length > 0) {
 
                     for (var i = 0; i < 5 && i < dataCast.length; i++) {
                       if (i == 0) {
                         cast += dataCast[i].name
-                      }else{
+                      } else {
 
                         cast += `, ${dataCast[i].name}`
                       }
@@ -260,37 +261,36 @@ let app = new Vue({
 
 
 
-              });
+            });
           }
 
 
-          // QUESTA PARTE NON MI PIACE PROPRIO PER NIENTE
 
           this.searchResultsTotal = this.searchResultsMovie.concat(this.searchResultsTV);
-          // console.log(this.searchResultsTotal);
-          // QUESTA PARTE NON MI PIACE PROPRIO PER NIENTE
         })
-        .catch(error =>{console.log(error)});
+        .catch(error => {
+          console.log(error)
+        });
     },
-    activemovie: function(){
+    activemovie: function() {
       this.sectionActive = 'movie';
       this.getSearch(this.searchText);
     },
-    activetv: function(){
+    activetv: function() {
       this.sectionActive = 'tv';
       this.getSearch(this.searchText);
     },
-    activeall: function(){
+    activeall: function() {
       this.sectionActive = 'all';
       this.getSearch(this.searchText);
 
     },
-    activegenre: function(val){
+    activegenre: function(val) {
       this.genreActive = val;
       this.getSearch(this.searchText);
 
     },
-    hamburgerButton: function(){
+    hamburgerButton: function() {
       this.hamburger = !this.hamburger;
     },
 
@@ -299,7 +299,6 @@ let app = new Vue({
   mounted() {
 
     this.initGenreMap();
-    // this.getSearch('ritorno');
 
 
   }
